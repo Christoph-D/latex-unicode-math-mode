ecaecbf33594bf2f2db7087ba0eb4741a1f25cfa
Christoph Dittmann <github@christoph-d.de>
2016-02-04 18:33:10 +0100

    Fix flycheck-package warnings

--- latex-unicode-math-mode.el
+++ latex-unicode-math-mode.el
@@ -6,6 +6,21 @@
 ;; Version: 0.1
 ;; URL: https://github.com/Christoph-D/latex-unicode-math-mode
 
+;;; Commentary:
+;; An Emacs minor mode for entering Unicode math symbols in LaTeX-mode
+;; (provided by AUCTeX).  It automatically replaces inputs like `\phi`
+;; with `φ` and `\in` with `∈`.  These replacements happen inside of
+;; math environments or everywhere, depending on the configuration.
+;;
+;; If you use pdflatex, you probably want to add
+;; `\usepackage{unicode-math-mode}` to your tex file in order to make
+;; pdflatex aware of the Unicode characters.  Use `M-x
+;; latex-unicode-save-sty-file` to put this file somewhere where
+;; pdflatex will find it.  If you update this package, you may need to
+;; do this again to get the latest sty file.
+;;
+;;; Code:
+
 (require 'robin)
 
 (robin-define-package
@@ -366,7 +381,8 @@ pdflatex."
     (message "Wrote %s" dest)))
 
 (defun latex-unicode-math-set-input-method ()
-  (if (texmathp) ;; If the point is inside a math environment
+  "Activate the input method iff point is in a math environment."
+  (if (texmathp)
       (activate-input-method 'math-symbols-tex)
     (deactivate-input-method)))
 
