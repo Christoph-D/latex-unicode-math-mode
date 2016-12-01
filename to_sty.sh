be207620dd6e753f6dda173f52e637b369b778d1
Christoph Dittmann <github@christoph-d.de>
2016-12-01 16:43:48 +0100

    Add a simple script to help generate the sty file

---
+++ to_sty.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+    cat <<EOF
+Usage: ./to_sty.sh <source-file>
+Converts lines of the form
+    ("\\aleph" ?ℵ)
+into
+    \DeclareUnicodeCharacter{2135}{\aleph}
+EOF
+    exit 1
+fi
+
+while read first rest; do
+    # Check and remove the leading question mark.
+    [[ $rest =~ ^\\?...$ ]] || continue
+    rest=${rest#?}
+    # Remove trailing paranthesis.
+    rest=${rest%)}
+    # Clean first.
+    first=${first#(\"}
+    first=${first%\"}
+    printf '\DeclareUnicodeCharacter{%04X}{%s}\n' "'$rest" "$first"
+done < "$1"
